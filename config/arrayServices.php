296406c60c47d176b4dc31345338870ce6756b8d
Roberta-Colombo <roberta.colombo89@hotmail.it>
2023-02-01 12:19:53 +0100

    Added filled array services + categories

---
+++ config/arrayServices.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+    [
+        'title' => 'Wi-Fi',
+        'img' => '<i class="fa-solid fa-wifi"></i>'
+    ],
+    [
+        'title' => 'Aria condizionata',
+        'img' => '<i class="fa-solid fa-fan"></i>'
+    ],
+    [
+        'title' => 'Garage',
+        'img' => '<i class="fa-solid fa-square-parking"></i>'
+    ],
+    [
+        'title' => 'Piscina',
+        'img' => '<i class="fa-solid fa-person-swimming"></i>'
+    ],
+    [
+        'title' => 'Idromassaggio',
+        'img' => '<i class="fa-solid fa-water-ladder"></i>'
+    ],
+    [
+        'title' => 'Portineria',
+        'img' => '<i class="fa-solid fa-bell-concierge"></i>'
+    ],
+    [
+        'title' => 'Sauna',
+        'img' => '<i class="fa-solid fa-spa"></i>'
+    ],
+    [
+        'title' => 'Palestra',
+        'img' => '<i class="fa-solid fa-dumbbell"></i>'
+    ],
+    [
+        'title' => 'Campo da tennis',
+        'img' => '<i class="fa-solid fa-table-tennis-paddle-ball"></i>'
+    ],
+    [
+        'title' => 'Campo da golf',
+        'img' => '<i class="fa-solid fa-golf-ball-tee"></i>'
+    ],
+    [
+        'title' => 'Giardino',
+        'img' => '<i class="fa-brands fa-pagelines"></i>'
+    ],
+    [
+        'title' => 'Patio',
+        'img' => '<i class="fa-solid fa-shop"></i>'
+    ],
+    [
+        'title' => 'Vista mare',
+        'img' => '<i class="fa-solid fa-water"></i>'
+    ],
+    [
+        'title' => 'Vigilanza notturna',
+        'img' => '<i class="fa-solid fa-person-military-pointing"></i>'
+    ],
+];
---
+++ config/arrrayCategories.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    [
+        'name' => 'Open space',
+        'img' => '<i class="fa-solid fa-person-shelter"></i>'
+    ],
+    [
+        'name' => 'Intera casa',
+        'img' => '<i class="fa-solid fa-house"></i>'
+    ],
+    [
+        'name' => 'Appartamento',
+        'img' => '<i class="fa-solid fa-city"></i>'
+    ],
+    [
+        'name' => 'Attico',
+        'img' => '<i class="fa-solid fa-people-roof"></i>'
+    ],
+    [
+        'name' => 'Villa di campagna',
+        'img' => '<i class="fa-regular fa-house-tree"></i>'
+    ],
+    [
+        'name' => 'Villa al mare',
+        'img' => '<i class="fa-solid fa-house-flood-water"></i>'
+    ],
+    [
+        'name' => 'Abitazione in stile industriale',
+        'img' => '<i class="fa-solid fa-warehouse"></i>'
+    ],
+    [
+        'name' => 'Abitazione in stile contemporaneo',
+        'img' => '<i class="fa-solid fa-building"></i>'
+    ],
+    [
+        'name' => 'Villa in stile romano',
+        'img' => '<i class="fa-solid fa-building-columns"></i>'
+    ],
+];
