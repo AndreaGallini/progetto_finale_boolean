52329c6cb64d0bd404a8b30956899b4f65f221f9
AndreGallini <andrea.gallini01@libero.it>
2023-02-03 11:18:34 +0100

    crud file

---
+++ database/seeders/MediabookSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MediabookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
