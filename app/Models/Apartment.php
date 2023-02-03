590a157469535e3636da1c6b277443db0cf6ac07
Francescolapiana <Lushika1219@gmail.com>
2023-02-03 18:25:27 +0100

    Models done 90%

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Apartment extends Model
 {
     use HasFactory;
+
+    public static function generateSlug($name)
+    {
+        return Str::slug($name, '-');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function stat(): BelongsTo
+    {
+        return $this->belongsTo(Stat::class);
+    }
+
+    // uno a uno
+    // public function mediabook(): BelongsTo
+    // {
+    //     return $this->belongsTo(Mediabook::class);
+    // }
+
+    public function service(): BelongsToMany
+    {
+        return $this->belongsToMany(Service::class, 'apartment_service', 'apartment_id', 'service_id');
+    }
+
+    public function sponsor(): BelongsToMany
+    {
+        return $this->belongsToMany(Sponsor::class, 'apartment_sponsor', 'apartment_id', 'sponsor_id');
+    }
 }
