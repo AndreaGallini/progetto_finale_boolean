1a1c13e8e0633606dd9e846b736031090326731b
AndreGallini <andrea.gallini01@libero.it>
2023-02-04 09:58:37 +0100

    add delete

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,4 +1,5 @@
 @foreach ($categories as $category)
     <h1>{{ $category->name }}</h1>
     <a href="{{ route('admin.category.edit', $category->slug) }}">modifica</a>
+    <a href="{{ route('admin.category.destroy', $category->slug) }}">cancella</a>
 @endforeach
--- resources/views/admin/service/index.blade.php
+++ resources/views/admin/service/index.blade.php
@@ -1,4 +1,5 @@
 @foreach ($services as $category)
     <h1>{{ $category->title }}</h1>
     <a href="{{ route('admin.category.edit', $category->slug) }}">modifica</a>
+    <a href="{{ route('admin.category.destroy', $category->slug) }}">cancella</a>
 @endforeach
