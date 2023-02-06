af57ab5050b4c46f90c3c6ba5120d64fb16c5f63
AndreGallini <andrea.gallini01@libero.it>
2023-02-06 15:33:17 +0100

    push

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -137,7 +137,7 @@
                 <div class="mb-3">
                     <h5>Visibility</h5>
                     <label for="visible" class="form-label">Visible</label>
-                    <input type="checkbox" class="form-check-input" id="visible" name="visible" value="0">
+                    <input type="checkbox" class="form-check-input" id="visible" name="visible" value="">
                     @error('visible')
                         <div class="invalid-feedback d-block">
                             {{ $message }}
