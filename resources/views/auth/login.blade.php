eb740cf377360970c081e43df037d2533c7b4000
LuongoAndrea <andrea.luongo2002@gmail.com>
2023-02-07 16:29:58 +0100

    prova 1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,11 +11,14 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="mb-4 row">
-                            {{-- <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label> --}}
+                        <div class="mb-4 row justify-content-center">
+                            {{-- <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail
+                                Address') }}</label> --}}
 
                             <div class="col-md-6">
-                                <input placeholder="Nome"  id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input placeholder="Mail" id="email" type="email"
+                                    class="form-control @error('email') is-invalid @enderror" name="email"
+                                    value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -25,11 +28,14 @@
                             </div>
                         </div>
 
-                        <div class="mb-4 row">
-                            {{-- <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label> --}}
+                        <div class="mb-4 row justify-content-center">
+                            {{-- <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password')
+                                }}</label> --}}
 
                             <div class="col-md-6">
-                                <input placeholder="la tua Email" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input placeholder="Password" id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    required autocomplete="current-password">
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -39,12 +45,13 @@
                             </div>
                         </div>
 
-{{-- remember me --}}
+                        {{-- remember me --}}
 
                         {{-- <div class="mb-4 row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{
+                                        old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
@@ -53,8 +60,8 @@
                             </div>
                         </div> --}}
 
-                        <div class="mb-4 row mb-0">
-                            <div class="col-md-8 offset-md-4 align-self-center d-flex">
+                        <div class="mb-4 row mb-0 justify-content-center">
+                            <div class="col-1 ">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
