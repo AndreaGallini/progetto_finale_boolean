2eed0f99b1f0e809fc6c44c2653e0f546825e5ea
AndreGallini <andrea.gallini01@libero.it>
2023-02-07 09:35:41 +0100

    add fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import "core-js/stable";
 import "babel-polyfill";
 import '~resources/scss/app.scss';
 import * as bootstrap from 'bootstrap';
+import axios from 'axios';
+import { forEach } from 'lodash';
 import.meta.glob([
     '../img/**'
 ])
@@ -13,12 +15,12 @@ const options = {
     idleTimePress: 100,
     minNumberOfCharacters: 0,
     searchOptions: {
-        key: 'mjOVKpgWnl7gsw0eNKkVguzisLjLZGIh',
+        key: 'h7cAdo65F2uuetiST0o1pnUygRaWDuuX',
         language: 'it-IT',
         limit: 5
     },
     autocompleteOptions: {
-        key: 'mjOVKpgWnl7gsw0eNKkVguzisLjLZGIh',
+        key: 'h7cAdo65F2uuetiST0o1pnUygRaWDuuX',
         language: 'it-IT'
     },
     noResultsMessage: 'No results found.'
@@ -66,16 +68,31 @@ if(document.getElementById('cover_image')){
 
 if(document.getElementById('searchBox')){
     let ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
+    let key = 'h7cAdo65F2uuetiST0o1pnUygRaWDuuX';
+
 
     let searchBoxHTML = ttSearchBox.getSearchBoxHTML();
     const searchBox = document.getElementById('searchBox');
     searchBox.appendChild(searchBoxHTML);
 
-    ttSearchBox.on('tomtom.searchbox.resultsfound', function(data) {
-        console.log(data);
+    ttSearchBox.on('tomtom.searchbox.resultsfound', function(event) {
+        console.log(event);
     })
 
-    ttSearchBox.on('tomtom.searchbox.resultselected', function(data) {
-        console.log(data);
+    ttSearchBox.on('tomtom.searchbox.resultselected', function(event) {
+      //  console.log(event.data.result.address.position);
+        let variabileindirizzo = document.getElementsByClassName("tt-search-box-input").value;
+        console.log(variabileindirizzo)
+        let selected = event.data.result.value
+        console.log(selected)
+        delete axios.defaults.headers.common['X-Requested-With'];
+        axios.get(`https://api.tomtom.com/search/2/geocode/${selected}.json?storeResult=false&view=Unified&key=h7cAdo65F2uuetiST0o1pnUygRaWDuuX`).then((res) => {
+            console.log(res.data.results)
+
+            return res.data
+        })
+
+
+
     })
 }
