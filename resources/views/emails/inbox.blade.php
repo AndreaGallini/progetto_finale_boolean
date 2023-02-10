226dba456f8e99c73c8c73817d411325dd78191e
GioGreco <grecogiorgio.97@gmail.com>
2023-02-10 10:58:23 +0100

    ffff

--- resources/views/emails/inbox.blade.php
+++ resources/views/emails/inbox.blade.php
@@ -3,7 +3,12 @@
     <h1>Ciaoooo</h1>
     @foreach ($apartments as $apartmentLead)
         <hr>
-        <p><strong>Mittente:</strong> {{ $apartmentLead->title }}</p>
+        <p><strong>Appartamento:</strong> {{ $apartmentLead->title }}</p>
+        <ul>
+            @foreach ($apartmentLead->leads as $lead)
+                <li>{{ $lead->message }}</li>
+            @endforeach
+        </ul>
 
         <hr>
     @endforeach
