6453b7106c12c3ef9e90bb8a92cf8f18314f5904
AndreGallini <andrea.gallini01@libero.it>
2023-02-13 10:50:20 +0100

    add footer

--- resources/views/partials/admin/footer.blade.php
+++ resources/views/partials/admin/footer.blade.php
@@ -1,1 +1,52 @@
-sono footer
+<div class="allfoot">
+    <div class="container">
+        <div class="row gy-3">
+            <div class="col-12 text-center">
+                <h1>
+                    Sito Realizzato da:
+                </h1>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 text-center">
+                <div class="creators">
+                    <div class="creator">
+                        <a href="https://github.com/GioGreco" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/108538293?v=4" alt="">
+                            GitHub <i class="fa-brands fa-github"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 text-center">
+                <div class="creators">
+                    <div class="creator">
+                        <a href="https://github.com/AndreaGallini" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/75275610?v=4" alt="">
+                            GitHub <i class="fa-brands fa-github"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 text-center">
+                <div class="creators">
+                    <div class="creator">
+                        <a href="https://github.com/Roberta-Colombo" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/108630368?v=4" alt="">
+                            GitHub <i class="fa-brands fa-github"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3 text-center">
+                <div class="creators">
+                    <div class="creator">
+                        <a href="https://github.com/FrancescolaPiana" target="_blank">
+                            <img src="https://avatars.githubusercontent.com/u/109112620?s=400&u=7b06baa474e925a91d92f3f88b2f81915d19d06e&v=4"
+                                alt="">
+                            GitHub <i class="fa-brands fa-github"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
