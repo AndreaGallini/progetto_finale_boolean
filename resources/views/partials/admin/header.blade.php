58621fff66619e71b34dcc14ae62e0b448511990
Francescolapiana <Lushika1219@gmail.com>
2023-02-15 16:36:08 +0100

    link shop header

--- resources/views/partials/admin/header.blade.php
+++ resources/views/partials/admin/header.blade.php
@@ -3,7 +3,7 @@
         <h1>BoolBnB</h1>
     </a>
             {{-- aggiungere link gome vue --}}
-    <a class="backtom" href="#">
+    <a class="backtom" target="_blank" href="http://localhost:5173">
         <i class="fa-solid fa-shop"></i>
     </a>
 </header>
