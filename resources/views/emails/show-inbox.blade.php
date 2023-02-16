6effa2a5100d28d7b872a6d456160401aa28e4d1
AndreGallini <andrea.gallini01@libero.it>
2023-02-16 16:36:12 +0100

    fix

--- resources/views/emails/show-inbox.blade.php
+++ resources/views/emails/show-inbox.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
 @section('content')
-
-<h1>Ciaoooo</h1>
-@foreach ($apartment->leads as $lead)
-    <hr>
-    <p><strong>Mittente:</strong> {{ $lead->name }}</p>
-    <p><strong>Id appartamento:</strong> {{ $lead->apartment_id }}</p>
-    <p><strong>Mail mittente:</strong> {{ $lead->email }}</p>
-    <p><strong>Messaggio:</strong> {{ $lead->message }}</p>
-    <hr>
-@endforeach
-{{-- <h2>{{ $apartment->title }}</h2> --}}
+    <h1>Ciaoooo</h1>
+    @foreach ($apartment->leads as $lead)
+        <hr>
+        <p><strong>Mittente:</strong> {{ $lead->name }}</p>
+        <p><strong>Id appartamento:</strong> {{ $lead->apartment_id }}</p>
+        <p><strong>Mail mittente:</strong> {{ $lead->email }}</p>
+        <p><strong>Messaggio:</strong> {{ $lead->message }}</p>
+        <hr>
+    @endforeach
+    {{-- <h2>{{ $apartment->title }}</h2> --}}
+@endsection
+@section('content')
+    @foreach ($apartment->leads as $lead)
+    @endforeach
 @endsection
