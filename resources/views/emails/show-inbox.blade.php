a1f62eeb6f31328b9559307bbc6941e2d3b77de5
AndreGallini <andrea.gallini01@libero.it>
2023-02-16 17:15:41 +0100

    fix

--- resources/views/emails/show-inbox.blade.php
+++ resources/views/emails/show-inbox.blade.php
@@ -1,15 +1,29 @@
 @extends('layouts.app')
 @section('content')
-<section>
-    <h1>Inbox di: </h1>
-    @foreach ($apartment->leads as $lead)
-        <hr>
-        <p><strong>Mittente:</strong> {{ $lead->name }}</p>
-        <p><strong>Id appartamento:</strong> {{ $lead->apartment_id }}</p>
-        <p><strong>Mail mittente:</strong> {{ $lead->email }}</p>
-        <p><strong>Messaggio:</strong> {{ $lead->message }}</p>
-        <hr>
-    @endforeach
-</section>
-{{-- <h2>{{ $apartment->title }}</h2> --}}
+    <h1>Inbox di: {{ $apartment->title }} </h1>
+    <section class="  my_dflex">
+
+        @foreach ($apartment->leads as $lead)
+            <div class=" my_col">
+                <div class="top_messages ">
+
+                    <p class="name_message"><strong>Mittente:</strong> {{ $lead->name }}</p>
+                    <p class="mail_message"><strong>Mail mittente:</strong> {{ $lead->email }}</p>
+
+
+                    <small onclick="dropdown({{ $lead->id }})">Clicca qui per vedere il corpo della mail</small>
+                </div>
+                <div class="bottom_messages" id="myDropdown">
+                    <p class="pt-2"><strong>Messaggio:</strong> {{ $lead->message }}</p>
+                </div>
+            </div>
+        @endforeach
+
+    </section>
+    {{-- <h2>{{ $apartment->title }}</h2> --}}
 @endsection
+<script>
+    function dropdown(id) {
+        document.getElementById('myDropdown').classList.toggle("show");
+    }
+</script>
